4bfc93d2f3df8261c6648d3532208cc37206091b
Isabella SMA <silvaisabella721@gmail.com>
2023-12-23 19:49:06 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,4 @@
+
+<a href=C:\Users\silva\Downloads\csharpCodes\imagemgithub.png><img width=40>
+##
+Neste repositório encontra-se códigos em C# autorais e resoluções de exercícios da Linguagem.
---
+++ Saudacao.cs
@@ -0,0 +1,6 @@
+﻿string nome;
+		
+		Console.WriteLine("Digite seu nome: ");
+		nome= Console.ReadLine();
+		Console.WriteLine("Olá, "+ nome);
+		Console.WriteLine ("Este é meu código de saudações!");
